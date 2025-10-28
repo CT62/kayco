2052ee32d6b0bfcaa50409804f9fdd56569f7c2b
clive <clivethompson@gmail.com>
2025-10-28 14:55:29 +0000

    commit

--- app/qr/route.ts
+++ app/qr/route.ts
@@ -1,16 +1,16 @@
 import { redirect } from 'next/navigation';
 import { cookies } from 'next/headers';
 
-const AUTH_COOKIE_NAME = 'site_authenticated';
+//const AUTH_COOKIE_NAME = 'site_authenticated';
 export async function GET() {
-  const cookieStore = await cookies();
-  cookieStore.set(AUTH_COOKIE_NAME, 'true', {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'strict',
-    maxAge: 60 * 60 * 24 * 7,
-    path: '/',
-  });
+  //const cookieStore = await cookies();
+  //cookieStore.set(AUTH_COOKIE_NAME, 'true', {
+    //httpOnly: true,
+    //secure: true,
+    //sameSite: 'strict',
+    //maxAge: 60 * 60 * 24 * 7,
+    //path: '/',
+  //});
 
   redirect('/');
 }
